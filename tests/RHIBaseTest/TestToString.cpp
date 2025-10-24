bbd1ac40ba9790ac6f20bb86a8f069ec44b62a09
ZilverBlade <90721817+ZilverBlade@users.noreply.github.com>
2025-10-25 00:11:00 +0200

    pretty please

--- tests/RHIBaseTest/TestToString.cpp
+++ tests/RHIBaseTest/TestToString.cpp
@@ -617,10 +617,10 @@ TEST(RHICommonToStringTests, FenceSubmitInfoToString) {
 }
 
 TEST(RHICommonToStringTests, CommandQueueSubmitInfoToString) {
-    ICommandQueue* testQueue = reinterpret_cast<ICommandQueue*>(0xDEADBEEF00000000ULL); // Ensure it's 64-bit for consistency
-    Semaphore waitSemaphore1 = reinterpret_cast<Semaphore>(0x11111111ULL);            // Explicitly construct Semaphore with u64
-    Semaphore signalSemaphore1 = reinterpret_cast<Semaphore>(0x22222222ULL);
-    IFence* signalFence1 = reinterpret_cast<IFence*>(0x3333333300000000ULL); // Ensure 64-bit
+    ICommandQueue* testQueue = reinterpret_cast<ICommandQueue*>(0x00000000DEADBEEFULL); // Ensure it's 64-bit for consistency
+    Semaphore waitSemaphore1 = reinterpret_cast<Semaphore>(0x11111111);            // Explicitly construct Semaphore with u64
+    Semaphore signalSemaphore1 = reinterpret_cast<Semaphore>(0x22222222);
+    IFence* signalFence1 = reinterpret_cast<IFence*>(0x0000000033333333ULL); // Ensure 64-bit
 
     CommandQueueSubmitInfo info = {};
     info.queue = testQueue;
